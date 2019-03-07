d9002c64dca7f304f380adce39496553d5d7af6a
Divyaansh Jain <divyaansh03anu@gmail.com>
2019-03-07 10:02:59 +0530

    Made a student class

---
+++ file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+	private:
+		
+			char name[20],regd[10],branch[10];
+			int sem;
+	public:
+		void input();
+		void display();
+			
+		
+};
